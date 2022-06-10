7c13adbae0bddbc9f8c47797fa88426b5f5fa73d
arya-sa <rizkyaryas11@gmail.com>
2022-06-10 08:03:32 +0700

    pagination

--- assets/js/main.js
+++ assets/js/main.js
@@ -94,11 +94,3 @@ for (let i = 0; i < document.links.length; i++) {
     }
 }
 document.links[current].className = 'current';
-
-
-for (let e of document.querySelectorAll('input[type="range"].slider-progress')) {
-  e.style.setProperty('--value', e.value);
-  e.style.setProperty('--min', e.min == '' ? '0' : e.min);
-  e.style.setProperty('--max', e.max == '' ? '100' : e.max);
-  e.addEventListener('input', () => e.style.setProperty('--value', e.value));
-}
