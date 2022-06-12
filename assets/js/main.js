1739821449cc58bc33eb3321e5cb9b3b07bb4c47
arya-sa <rizkyaryas11@gmail.com>
2022-06-12 14:42:23 +0700

    blog

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,34 +25,6 @@ function closeMenu() {
   navMenu.classList.remove("active");
 }
 
-// Slider
-
-// let slideIndex = 1;
-// console.log(slideIndex)
-// showSlides(slideIndex);
-
-// function showSlides(n) {
-//   let i;
-//   let slides = document.getElementsByClassName("slider-card");
-// //   let dots = document.getElementsByClassName("dot");
-//   if (n > slides.length) {slideIndex = 1}
-//   if (n < 1) {slideIndex = slides.length}
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//   }
-// //   for (i = 0; i < dots.length; i++) {
-// //     dots[i].className = dots[i].className.replace(" active", "");
-// //   }
-//   slides[slideIndex-1].style.display = "block";
-// //   dots[slideIndex-1].className += " active";
-// }
-// function plusSlides(n) {
-//   showSlides(slideIndex += n);
-// }
-
-// function currentSlide(n) {
-//   showSlides(slideIndex = n);
-// }
 
 // Modal popup
 
@@ -204,4 +176,48 @@ dropdownList.forEach((n) => n.addEventListener("click", closeDropdown));
 
 function closeDropdown() {
   dropdownBody.classList.remove("open")
+}
+
+// Filter Blog
+
+filterSelection("all")
+function filterSelection(c) {
+  let x, i;
+  x = document.getElementsByClassName("blog-card");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    removeClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) addClass(x[i], "show");
+  }
+}
+
+function addClass(element, name) {
+  let i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+  }
+}
+
+function removeClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);     
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+// Add active class to the current button (highlight it)
+const filterBtn = document.getElementsByClassName("news");
+for (let i = 0; i < filterBtn.length; i++) {
+  filterBtn[i].addEventListener("click", function(){
+    const current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
 }
