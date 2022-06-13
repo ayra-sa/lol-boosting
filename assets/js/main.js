d8474d537f04df2dc9585bf6739f9eb536bff9a6
arya-sa <rizkyaryas11@gmail.com>
2022-06-13 16:48:48 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -249,3 +249,16 @@ for (let i = 0; i < filterBtn.length; i++) {
     this.className += " active";
   });
 }
+
+// Show Password
+
+const togglePassword = document.querySelector('#togglePassword');
+const password = document.querySelector('#id_password');
+
+togglePassword.addEventListener('click', function (e) {
+  // toggle the type attribute
+  const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+  password.setAttribute('type', type);
+  // toggle the eye slash icon
+  this.classList.toggle('fa-eye-slash');
+});
